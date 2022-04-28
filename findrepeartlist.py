97fea4ec582ea8d6e4bf760ede38418db0679f5b
greenfinchbaby@gmail.com <greenfinchbaby@gmail.com>
2022-04-29 00:04:06 +0800

    完成打开文件，去除前面不需要内容，得到并遍历纯jason和每一个列表

--- findrepeartlist.py
+++ findrepeartlist.py
@@ -41,12 +41,30 @@ def appendPathToFilename(names):
 
 #打开单个lpl文件查找重复首字段内容
 def findRepeatInLplFile(filePath = playlistsPath+"MAME.lpl"):
+ pure_lists = openFileGetPureJson(filePath)
+ json_lists = json.loads(pure_lists)
+
+ for onelist in json_lists:
+  print(onelist)
+
+ # for key in pure_load_f:
+ #  print('======key========:' + key + "************value******" + key)
+
+#打开文件去掉前面不需要信息，获取纯内容
+def openFileGetPureJson(filePath):
  with open(filePath, 'rb') as load_f:
   load_dict = json.load(load_f)
-  print(load_dict)
+  pure_load_f = delFromNToEnd(load_dict)
+  return pure_load_f
 
-
-
+#截取掉前面多余部分{'version': '1.4', 'default_core_path': '', 'default_core_name': '', 'label_display_mode': 0, 'right_thumbnail_mode': 0, 'left_thumbnail_mode': 0, 'sort_mode': 0, 'items':
+def delFromNToEnd(input_jason):
+  #jason文件转为字符串
+  input_string = json.dumps(input_jason)
+  length = len("{'version': '1.4', 'default_core_path': '', 'default_core_name': '', 'label_display_mode': 0, 'right_thumbnail_mode': 0, 'left_thumbnail_mode': 0, 'sort_mode': 0, 'items':{")
+  subinputstring = input_string[length:-1]
+  print(subinputstring)
+  return subinputstring
 
  # with open("./record.json", "w") as dump_f:
  #  json.dump(load_dict, dump_f)
